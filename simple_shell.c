a4400547c9f83082d681e033349df74c80da1962
DemisoDaba <demo.nkmt1@gmail.com>
2023-05-11 22:08:21 -0700

    Adding simple_shell files

---
+++ simple_shell.c
@@ -0,0 +1,14 @@
+#include "shell_header.h"
+
+
+/**
+ * main - main function
+ * @argc: amount of arguments
+ * @argv: arguments
+ * Return: always 0
+ */
+int main(__attribute__((unused)) int argc, __attribute__((unused)) char **argv)
+{
+	loop_shell();
+	return (0);
+}
