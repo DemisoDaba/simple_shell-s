0001b5b9f968b069fbe802461dc10b7c30266a5b
DemisoDaba <demo.nkmt1@gmail.com>
2023-05-11 22:18:58 -0700

    Preparing function shell

---
+++ function.c
@@ -0,0 +1,9 @@
+#include "shell_header.h"
+
+/**
+ * function_cd - verify if argument is a existing directory,
+ * @args: argument
+ * if not have arguments ask for one
+ * Return: always 1
+ */
+int function_cd(char **args)
