76f8ee6a43b0473cfdd5499987c75bf72ec05114
DemisoDaba <demo.nkmt1@gmail.com>
2023-05-13 08:26:58 -0700

    Updating line 18 such that,the function can compare two strings

--- stringcmp.c
+++ stringcmp.c
@@ -4,6 +4,7 @@
  * stringcmp - Compare two strings similar to strcmp
  * @stringA: The first string to be compared
  * @stringB: The second string to be compared
+ * @a: Limit of the index at which we want to compare both strings.
  *
  * Return: 0 if stringA and stringB are exactly equal,
  *         positive value if stringA is greater,
@@ -14,7 +15,8 @@ int stringcmp(char *stringA, char *stringB, int a)
 {
 	int count;
 
-	for (count = 0; (count < a) && (stringA[count] != '\0') && (stringB[count] != '\0'); count++)
+	for (count = 0; (count < a) && (stringA[count] != '\0') &&
+			(stringB[count] != '\0'); count++)
 	{
 		if (stringA[count] != stringB[count])
 		return (stringA[count] - stringB[count]);
