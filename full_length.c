3babef518ddd1d18e9db7fec1d1d21e4e833e2cc
DemisoDaba <demo.nkmt1@gmail.com>
2023-05-14 04:34:33 -0700

    A fuction that returns the sum of lengths of two array of pointers

---
+++ full_length.c
@@ -0,0 +1,25 @@
+#include "shell.h"
+
+/**
+ * full_length - Calculates the combined length of two arrays of strings.
+ * @stringsA: The first array of strings.
+ * @stringsB: The second array of strings.
+ *
+ * Return: The total length of stringsA and stringsB.
+ */
+
+int full_length(char **stringsA, char **stringsB)
+{
+	int countA, countB;
+
+	for (countA = 0; stringsA[countA] != NULL; countA++)
+	{
+
+	}
+
+	for (countB = 0; stringsB[countB] != NULL; countB++)
+	{
+
+	}
+	return (countA + countB);
+}
