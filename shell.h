b99a1d5d7d520196f1315bbfafeae5e9dac4afd1
DemisoDaba <demo.nkmt1@gmail.com>
2023-05-15 09:41:54 -0700

    Adding all prototypes

--- shell.h
+++ shell.h
@@ -16,3 +16,28 @@ char *get_path(char **envp);
 void print(char *string);
 char *get_user_input();
 #endif /* End of shell_h */
+
+#ifndef shell_h
+#define shell_h
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdbool.h>
+#include <errno.h>
+#define BUFFER_SIZE 1024
+
+int stringcmp(char *StringA, char *StringB, int a);
+char *get_user_input();
+char **tokenize(char *user_input, char *delimiter);
+int stringlen(char *String);
+void print(char *String);
+char *get_path(char **envp);
+int full_length(char **StringsA, char **StringsB);
+char *stringdup(char *StringToDup);
+char *stringcat(char *CompleteString, char *StringToAdd);
+char **full_path(char **tok_path, char **tok_user_input);
+char **init_envp(char **envp);
+char *stringcpy(char *StringDest, char *StringSource);
+
+#endif /* End of shell_h */
