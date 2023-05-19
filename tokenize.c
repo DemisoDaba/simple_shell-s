838d55b84b115c75342f83d2658e941a4740c6f4
Demiso Daba <125874545+DemisoDaba@users.noreply.github.com>
2023-05-19 09:37:14 +0300

    Updated

--- tokenize.c
+++ tokenize.c
@@ -3,7 +3,7 @@
 /**
  * tokenize - Tokenizes a string of user input
  * @user_input: The input string to tokenize
- * @separator: The separator used for tokenization
+ * @delimiter: The separator used for tokenization
  *
  * This function takes a string of user input and tokenizes it
  * It returns a double pointer to an array of tokens, or NULL on failure.
@@ -11,46 +11,53 @@
  * Return: A double pointer to an array of tokens, or NULL on failure
  */
 
-char **tokenize(char *user_input, char *separator)
+char **tokenize(char *user_input, char *delimiter)
 {
-	int tokenCount = 0, MaxNumOfTok = BUFFER_SIZE;
-	char *currentToken = NULL;
-	char **tokenArray = malloc(sizeof(char *) * MaxNumOfTok);
+    int NumOfTok = 0, MaxNumOfTok = BUFFER_SIZE;
+    char *Token = NULL;
+    char **Tokens = malloc(sizeof(char *) * (MaxNumOfTok + 1));
 
-	if (tokenArray == NULL)
-	{
-		perror("Malloc failed to allocate memory for storing tokens");
-		return (NULL);
-	}
-	else
-	{
-		currentToken = strtok(user_input, separator);
+    if (Tokens == NULL)
+    {
+        perror("Malloc failed to allocate memory for storing tokens");
+        return NULL;
+    }
+    else
+    {
+        Token = strtok(user_input, delimiter);
 
-		if (currentToken == NULL)
-		{
-			perror("Strtok failed to tokenize user input");
-			return (tokenArray);
-		}
-		else
-		{
-
-			while (currentToken != NULL)
-			{
-				if (tokenCount == MaxNumOfTok)
-				{
-					MaxNumOfTok = MaxNumOfTok * 2;
-					tokenArray = realloc(tokenArray, MaxNumOfTok * sizeof(char *));
-					if (tokenArray == NULL)
-					{
-					perror("Realloc failed to reallocate memory for storing token");
-					free(tokenArray);
-					return (NULL);
-					}
-				}
-				tokenArray[tokenCount++] = currentToken;
-				currentToken = strtok(NULL, separator);
-			}
-		}
-	}
-	return (tokenArray);
+        if (Token == NULL)
+        {
+            perror("Strtok failed to tokenize user input");
+        }
+        else
+        {
+            while (Token != NULL)
+            {
+                if (NumOfTok == MaxNumOfTok)
+                {
+                    MaxNumOfTok = MaxNumOfTok * 2;
+                    Tokens = realloc(Tokens, (MaxNumOfTok + 1) * sizeof(char *));
+                    if (Tokens == NULL)
+                    {
+                        perror("Realloc failed to reallocate memory for storing token");
+                        free(Tokens);
+                        return (NULL);
+                    }
+                }
+                Tokens[NumOfTok] = malloc(stringlen(Token));  // Allocate memory for token
+                if (Tokens[NumOfTok] == NULL)
+                {
+                    perror("Malloc failed to allocate memory for token");
+                    free(Tokens);
+                    return NULL;
+                }
+                stringcpy(Tokens[NumOfTok], Token);  // Copy token contents
+                NumOfTok++;
+                Token = strtok(NULL, delimiter);
+            }
+        }
+    }
+    Tokens[NumOfTok] = NULL;  // Set the last element of Tokens to NULL
+    return Tokens;
 }
