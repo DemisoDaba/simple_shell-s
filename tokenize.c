5a448b6f28f0ed8b7d1cc50b3ae8d53d99be4848
DemisoDaba <demo.nkmt1@gmail.com>
2023-05-22 10:08:20 -0700

    Updated

--- tokenize.c
+++ tokenize.c
@@ -45,20 +45,22 @@ char **tokenize(char *user_input, char *delimiter)
                         return (NULL);
                     }
                 }
-                Tokens[NumOfTok] = malloc(stringlen(Token));  // Allocate memory for token
+                Tokens[NumOfTok] = malloc(stringlen(Token));  /* Allocate memory for token */
+                Tokens[NumOfTok] = malloc(stringlen(Token));
                 if (Tokens[NumOfTok] == NULL)
                 {
                     perror("Malloc failed to allocate memory for token");
                     free(Tokens);
                     return NULL;
                 }
-                stringcpy(Tokens[NumOfTok], Token);  // Copy token contents
+                stringcpy(Tokens[NumOfTok], Token);  /* Copy token contents */
+                stringcpy(Tokens[NumOfTok], Token);
                 NumOfTok++;
                 Token = strtok(NULL, delimiter);
             }
         }
     }
-    Tokens[NumOfTok] = NULL;  // Set the last element of Tokens to NULL
+    Tokens[NumOfTok] = NULL;  /* Set the last element of Tokens to NULL */
+    Tokens[NumOfTok] = NULL;
     return Tokens;
 }
-
