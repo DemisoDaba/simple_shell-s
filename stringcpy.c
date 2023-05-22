4185bbaf620119199d452428b7f1cd63f443a85f
DemisoDaba <demo.nkmt1@gmail.com>
2023-05-22 11:10:05 -0700

    Betty correction

--- stringcpy.c
+++ stringcpy.c
@@ -1,11 +1,12 @@
 #include "shell.h"
 
 /**
- * Copies the contents of one string to another.
+ * stringcpy - Copy the contents of one string to another.
  *
- * @param StringDest     Pointer to the destination string where the contents will be copied.
- * @param StringSource   Pointer to the source string whose contents will be copied.
- * @return               Pointer to the destination string.
+ * @StringDest: Pointer to the destination string.
+ * @StringSource: Pointer to the source string.
+ *
+ * Return: Pointer to the destination string.
  */
 
 char *stringcpy(char *StringDest, char *StringSource)
